28a6de1b4c2fd7ae69743265696f02728bfa115a
Christian de Abreu Borges <christianborges27@hotmail.com>
2025-03-13 11:35:29 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoPedido/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using ProjetoPedido.Entities.Enum;
+
+namespace ProjetoPedido.Entities.Enum
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ ProjetoPedido/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using ProjetoPedido.Entities;
+using ProjetoPedido.Entities.Enum;
+
+namespace ProjetoPedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                // CRIA O PRODUTO
+                Console.WriteLine($"Enter #{i + 1} item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, productPrice);
+
+                // CRIA O ITEM DO PEDIDO
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+
+        }
+
+    }
+}
